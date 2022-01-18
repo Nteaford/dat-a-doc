920c5b22d1113a346aed851ab102a6de67880eaa
Nick Teaford <nickteaford@Nicks-iMac.local>
2022-01-18 11:39:57 -0800

    create.html page

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('appointment/', views.appointment_index, name='appointment_index'),
+    path('appointment/create/', views.appointment_create_specialty, name='appointment_create_specialty'),
+    path('appointment/create/<str:selected_specialty>/', views.appointment_create_doctor, name='appointment_create_doctor'),
+    path('appointment/create/<str:selected_specialty>/<int:doctor_id>/', views.appointment_create_appointment, name='appointment_create_appointment'),
 
 ]
